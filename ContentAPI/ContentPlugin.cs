1a447e6abebf23712da1ba5ced5df08fbe0ecabc
Nameless <85962933+Misfiy@users.noreply.github.com>
2024-12-02 14:33:00 +0100

    Initial commit

---
+++ ContentAPI/ContentPlugin.cs
@@ -0,0 +1,27 @@
+﻿namespace ContentAPI
+{
+    using BepInEx;
+
+    /// <summary>
+    /// Base class handling loading the plugin.
+    /// </summary>
+    [BepInPlugin(ContentGUID, ContentName, ContentVersion)]
+    public class ContentPlugin : BaseUnityPlugin
+    {
+        private const string ContentGUID = "Circus.ContentAPI";
+        private const string ContentName = "ContentAPI";
+        private const string ContentVersion = "1.0.0";
+
+        /// <summary>
+        /// Gets the instance of the plugin.
+        /// </summary>
+        public static ContentPlugin Instance { get; private set; }
+
+        private void Awake()
+        {
+            Instance = this;
+
+            Logger.LogInfo($"Plugin {ContentGUID}@{ContentVersion} is loaded!");
+        }
+    }
+}
